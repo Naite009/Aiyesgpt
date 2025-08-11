be1ae5bd09e2a2f495f1a645c9eb8af9710bab3e
Nathan Newman <natenewman@gmail.com>
2025-08-11 19:32:57 +0000

    routes: support /student/guided/:id and /guided/:id + SPA rewrites

--- src/main.tsx
+++ src/main.tsx
@@ -7,34 +7,23 @@ import App from "./App";
 import StudentLayout from "./layouts/StudentLayout";
 import TeacherLayout from "./layouts/TeacherLayout";
 
-// Pages (existing)
+// Pages
 import Browse from "./pages/Browse";
 import Favorites from "./pages/Favorites";
 import Create from "./pages/Create";
 import Studio from "./pages/Studio";
 import Lessons from "./pages/Lessons";
+import Guided from "./pages/Guided";
 import TestMode from "./pages/TestMode";
 
 import "./index.css";
-
-import Guided from "./pages/Guided"; // must exist with export default
-
-<Route path="student" element={<StudentLayout />}>
-  <Route index element={<Navigate to="browse" replace />} />
-  <Route path="browse" element={<Browse />} />
-  <Route path="favorites" element={<Favorites />} />
-  <Route path="guided/:id" element={<Guided />} />  {/* <-- This is the one */}
-  <Route path="test/:id" element={<TestMode />} />
-</Route>
-
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Root shell */}
         <Route path="/" element={<App />}>
-          {/* Default → Student browse */}
+          {/* Default → student browse */}
           <Route index element={<Navigate to="/student/browse" replace />} />
 
           {/* Student area */}
@@ -54,12 +43,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
             <Route path="lessons" element={<Lessons />} />
           </Route>
 
-          {/* Legacy paths → keep old links working */}
+          {/* Support legacy direct paths too */}
+          <Route path="guided/:id" element={<Guided />} />
+          <Route path="test/:id" element={<TestMode />} />
           <Route path="browse" element={<Navigate to="/student/browse" replace />} />
           <Route path="favorites" element={<Navigate to="/student/favorites" replace />} />
-          <Route path="guided/:id" element={<Navigate to="/student/guided/:id" replace />} />
-          <Route path="test/:id" element={<Navigate to="/student/test/:id" replace />} />
-
           <Route path="create" element={<Navigate to="/teacher/create" replace />} />
           <Route path="studio" element={<Navigate to="/teacher/studio" replace />} />
           <Route path="lessons" element={<Navigate to="/teacher/lessons" replace />} />
